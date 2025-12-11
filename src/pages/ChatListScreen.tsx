39ab4e0cb27e32eb26d104ed9547dceea4788845
jayeshrraval4-eng <jayeshrraval4@gmail.com>
2025-12-11 22:27:25 +0530

    Update ChatListScreen.tsx

--- src/pages/ChatListScreen.tsx
+++ src/pages/ChatListScreen.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { MessageSquare, Clock } from "lucide-react";
 
-https://samaj-backend-nqjq.onrender.com
 
 function getCurrentUser() {
   try {

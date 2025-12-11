9e8991712ba0512609696516a4dba8f439ba8d5f
jayeshrraval4-eng <jayeshrraval4@gmail.com>
2025-12-11 22:29:10 +0530

    Update ProfileScreen.tsx

--- src/pages/ProfileScreen.tsx
+++ src/pages/ProfileScreen.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import BottomNav from "../components/BottomNav";
 
-https://samaj-backend-nqjq.onrender.com
 
 type Profile = {
   id?: number;

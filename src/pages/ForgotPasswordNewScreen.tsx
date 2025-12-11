6c62ac01417e8171a0b4d84cf399efaea772c128
jayeshrraval4-eng <jayeshrraval4@gmail.com>
2025-12-11 22:27:59 +0530

    Update ForgotPasswordNewScreen.tsx

--- src/pages/ForgotPasswordNewScreen.tsx
+++ src/pages/ForgotPasswordNewScreen.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { useSearchParams, useNavigate } from "react-router-dom";
 
-https://samaj-backend-nqjq.onrender.com
 
 export default function ForgotPasswordNewScreen() {
   const [params] = useSearchParams();
